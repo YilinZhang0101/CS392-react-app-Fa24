009e80814a6d655a5b0e5f86dc6d1a62338f9d0a
yilinZhang0101 <yilin19@illinois.edu>
2024-10-10 00:54:19 -0400

    term filter

---
+++ src/components/MenuPage.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const terms = {
+  Fall: 'Fall',
+  Spring: 'Spring',
+  Winter: 'Winter'
+};
+
+const TermButton = ({term, selection, setSelection}) => (
+  <div>
+    <input type="radio" id={term} className="btn-check" checked={term === selection} autoComplete="off"
+      onChange={() => setSelection(term)} />
+    <label className="btn btn-success mb-1 p-2" htmlFor={term}>
+    { term }
+    </label>
+  </div>
+);
+
+const TermSelector = ({selection, setSelection}) => (
+  <div className="btn-group">
+    { 
+      Object.keys(terms).map(term => <TermButton key={term} term={term} selection={selection} setSelection={setSelection} />)
+    }
+  </div>
+);
+
+const MenuPage = () => {
+  const [selection, setSelection] = useState(() => Object.keys(terms)[0]);
+  return (
+    <div>
+      <TermSelector selection={selection} setSelection={setSelection} />
+      <p className="card">Term: {selection}</p>
+      <CourseList courses={courses} term={selection}></CourseList>
+    </div>
+  );
+}
+
+export default MenuPage;
